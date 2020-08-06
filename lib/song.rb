a243be2cd1274c3b4d4bb626fdd0e0f9b94e11aa
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-08-06 01:21:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   def self.genre_count
     genre_count = {}
     self.genres.each do |genre|
-      if genre_count[genre]
+    if genre_count[genre]
         genre_count[genre] +=1
       else genre_count[genre] = 1
     end
